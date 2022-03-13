ba967e7585779f9664ab8a06628013a5c6010ded
flics04 <flics04@gmail.com>
2022-03-13 11:20:31 +0800

    P1307 another

--- Part1/Chapter4/P1307.cpp
+++ Part1/Chapter4/P1307.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
-int n, s = 0;
 
 int main() {
-    cin >> n;
-    while (n)
-        //如果n不是0，就一直s让一位，腾个0出来，n最后一位跟上去，舍去最后一位
-        s = s * 10 + n % 10, n /= 10;
-    cout << s;
+    long long n, a = 0;        // 用long long是担心数据会炸
+    scanf("%lld", &n);
+
+    while (n) {
+        a = a * 10;         //a在十进制中左移一位
+        a = a + n % 10;     //a加上n的个位数（原来的个位数变成了现在的最高位数
+        n /= 10;
+    }
+    printf("%lld", a);//输出
     return 0;
 }
